8234f44eefaa90831477b95167283c16ac5673b2
PaulNdrei <andreipetrea3@gmail.com>
2022-11-16 13:36:22 +0100

    Demo branded

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,19 @@ FROM python:3.8-slim
 
 #RUN mkdir /home/spacy_demo
 
+RUN apt-get update && apt-get install --no-install-recommends -y git && \
+     rm -rf /var/lib/{apt,dpkg,cache,log}
+
+RUN python -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+
 WORKDIR /usr/src/app
+
+COPY .streamlit /usr/src/app/.streamlit
 
 COPY requirements.txt streamlit_app.py ./
 
-RUN pip install -r requirements.txt \
-    && rm -rf /root/.cache/pip
+RUN pip install --no-cache-dir -r requirements.txt
 
 #COPY . .
 
@@ -18,4 +25,4 @@ RUN pip install -r requirements.txt \
 #RUN python -m  pip install https://huggingface.co/spacy/es_core_news_md/resolve/main/es_core_news_md-any-py3-none-any.whl
 
 #streamlit run streamlit_app.py --server.port 8081 --theme.font serif --browser.serverPort 8081 --browser.serverAddress http://aina.bsc.es
-ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port", "8081", "--browser.serverAddress", "0.0.0.0", "--theme.font", "serif"]
+ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port", "8081", "--browser.serverAddress", "0.0.0.0"]
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,12 @@ import spacy_streamlit
 import streamlit as st
 
 models = ["es_core_news_md", "es_dep_news_trf"]  # ,"ca_core_news_lg","ca_core_news_trf"]
-st.set_page_config(page_title="Modelos Spacy castellano", page_icon="aina_small.jpg", layout='wide',
-                   initial_sidebar_state='auto')
+st.set_page_config(page_title="Modelos Spacy castellano", header_title="Spacy", menu_items={
+    'Get Help': 'https://huggingface.co/PlanTL-GOB-ES',
+    'Report a bug': 'https://github.com/TeMU-BSC/spacy_es_demo/issues',
+    'About': None,
+
+}, layout='wide',initial_sidebar_state='auto')
 default_text = "Los últimos datos confirman una buena evolución de la pandemia en España."
 st.title("Demo de las cadenas de Spacy 3.4 en castellano")
 st.subheader("*Elegir un modelo en la columna de la derecha*")
@@ -19,5 +23,8 @@ visualizers = ["ner", "similarity", "tokens", "parser", "textcat"]
 similarity_texts = ('gato', 'perro')
 spacy_streamlit.visualize(models, default_text, visualizers, similarity_texts=('perro', 'gato'),
                           show_visualizer_select=True, sidebar_title="Visualización modelos Spacy-PlanTL",
-                          sidebar_description="Es posible elegir el modelo y la funcionalidad para la demostración")
+                          sidebar_description="Es posible elegir el modelo y la funcionalidad para la demostración",
+                          ner_expander_text="Selecciona las etiquetas de  entidades",
+                          ner_entity_labels_text="Etiquetas de entidades"
+                          )
 # streamlit run streamlit_app.py --server.port 8081 --theme.font serif --browser.serverPort 8081 --browser.serverAddress http://aina.bsc.es
